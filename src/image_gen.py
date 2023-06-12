cc5885f27f2c352248ba0cbbc2b59cc8a86ac258
vojtech zahradnik <vojtech.zahradnik@adastragrp.com>
2023-06-12 22:37:33 +0200

    api completed

--- src/image_gen.py
+++ src/image_gen.py
@@ -1,27 +1,54 @@
 import requests
 import os
 from datetime import datetime
+from PIL import Image
+from io import BytesIO
+import glob
+
 
 class ImageGen:
 
-    def __init__(self, key: str) -> None:
+    def __init__(self, key: str, conf: dict) -> None:
         self.api_key = key
+        self.conf = conf
         self.image = None
 
-    def save_image(self, path="generated_imgs") -> bool:
-        # TODO try catch return bool - exception handling
-        self.image.save(os.path.join(path,datetime.now().strftime("%Y%m%d%H%M%S")))
+    def save_image(self) -> bool:
+        # TODO: try catch return bool - exception handling
+        response = requests.get(self.image["output_url"])
+        image = Image.open(BytesIO(response.content))
 
-    def gen_image(self, image_path: str, prompt: str) -> str:
-        # TODO exception to bad request, api error, no internet
+        if not os.path.exists(self.conf["img_dest"]):
+            os.mkdir(self.conf["img_dest"])
 
-        return requests.post(
-            "https://api.deepai.org/api/image-editor",
+        image.save(os.path.join(self.conf["img_dest"],
+                                f'{datetime.now().strftime("%Y%m%d%H%M%S")}.{self.conf["source_file_type"]}'),
+                   "PNG")
+        print("saved")
+
+    def gen_image(self, prompt) -> str:
+        # TODO: exception to bad request, api error, no internet
+
+        print("gen. started")
+        print(prompt)
+        self.image = requests.post(
+            self.conf["api_name"],
             files={
-                'image': open(image_path, 'rb'),
+                'image': open(self.get_last_png(), 'rb'),
                 'text': prompt,
             },
             headers={'api-key': self.api_key}
-        )
+        ).json()
+        print(self.image)
+        print("gen. done")
 
+    def get_last_png(self) -> str:
 
+        if not os.path.exists(self.conf["img_source"]):
+            os.mkdir(self.conf["img_source"])
+            return 1
+
+        png_files = glob.glob(os.path.join(self.conf["img_source"],
+                                           f'*.{self.conf["source_file_type"]}'))
+        png_files.sort(key=os.path.getmtime)
+        return png_files[-1]
--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
+import os
+import sys
+import yaml
+from image_gen import ImageGen
 
+# TODO: logging
+# TODO: image name is right?
+# TODO: mega testing!
+
+def load_config(path: str) -> dict:
+    with open(path, 'r') as file:
+        return yaml.safe_load(file)
 
 def main():
-    print("main")
+    image_gen = ImageGen(key=creds["DEEPAI_API_KEY"], conf=conf)
+    image_gen.gen_image(prompt="put me on the moon")
+    image_gen.save_image()
 
 if __name__ == "__main__":
-    main()
+
+    conf = load_config("../configs/conf.yaml")
+    creds = load_config("../configs/creds.yaml")
+
+    sys.exit(main())
