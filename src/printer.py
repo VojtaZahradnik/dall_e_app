a89dfa86ac2b60f32d854dc8ef3c5a8a6ac182c5
vojtech zahradnik <vojtech.zahradnik@adastragrp.com>
2023-06-19 14:09:47 +0200

    requirements + printer

--- src/printer.py
+++ src/printer.py
@@ -1,5 +1,6 @@
-# import win32print
+import win32printing
 from PIL import Image
+from subprocess import CalledProcessError
 
 PHYSICALWIDTH = 110
 PHYSICALHEIGHT = 111
@@ -10,26 +11,6 @@ class Printer:
         pass
         # self.default_printer = win32print.GetDefaultPrinter ()
 
-    def print_image(self, image):
-        # Convert the image to a Windows bitmap
-        bmp = image.convert("RGB").tobytes("raw", "BGR")
-        #
-        # # Open the printer
-        # hPrinter = win32print.OpenPrinter(self.default_printer)
-        #
-        # # Set the properties of the document
-        # job = win32print.StartDocPrinter(hPrinter, 1, ("test document", None, "RAW"))
-        #
-        # # Start a page
-        # win32print.StartPagePrinter(hPrinter)
-        #
-        # # Write the image data to the printer
-        # win32print.WritePrinter(hPrinter, bmp)
-        # # End the page
-        # win32print.EndPagePrinter(hPrinter)
-        #
-        # # End the document
-        # win32print.EndDocPrinter(hPrinter)
-        #
-        # # Close the printer
-        # win32print.ClosePrinter(hPrinter)
+    def print_image(self, img_path: str):
+        try:
+            subprocess.run(["mspaint.exe","/pt", img_path])
